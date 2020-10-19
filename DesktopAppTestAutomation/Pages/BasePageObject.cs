d06f035696249113148f4622147ed592634a6cb9
Ilya Horatio <34651650+ilyahoratio@users.noreply.github.com>
2020-10-19 23:11:40 +0300

    refactored page object for add cards tab, no more .gitignore for App.config

--- DesktopAppTestAutomation/Pages/BasePageObject.cs
+++ DesktopAppTestAutomation/Pages/BasePageObject.cs
@@ -1,7 +1,16 @@
-﻿namespace DesktopAppTestAutomation.Pages
+﻿using OpenQA.Selenium.Appium.Windows;
+
+namespace DesktopAppTestAutomation.Pages
 {
-    public static class BasePageObject
+    internal class BasePageObject
     {
+        private WindowsDriver<WindowsElement> WinDriver { get; set; }
+
+        public BasePageObject(WindowsDriver<WindowsElement> winDriver)
+        {
+            WinDriver = winDriver;
+        }
+
         public static string tabNameAddCards = "Add Cards";
         public static string tabNameBrowseCards = "Browse Cards";
     }
